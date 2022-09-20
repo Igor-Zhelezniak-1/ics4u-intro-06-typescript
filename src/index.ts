df4151291e7366192cd5ebc0631ae86da995ef25
Ihor <ihor.zhelezniak@mths.ca>
2022-09-20 13:46:22 +0000

    -

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * author  Igor Zhelezniak
+ * version 1.0
+ * since  2022-09-20
+ */
+
+console.log('Hello, World!')
